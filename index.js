aa6f30cebcf7af2b00445c6c9cf849e857b0a242
Dario Zubaray <dzubaray@gmail.com>
2018-12-17 10:55:43 -0300

    implementando swagger en fastify

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const fastify = require('fastify')({
   logger: true
 });
 const mongoose = require('mongoose');
-const routes = require('./src/routes')
+const routes = require('../src/routes');
+const swagger = require('./config/swagger');
 
 const port = process.env.PORT || 3000;
 const urlDB = ‘mongodb://localhost/mygarage’;
@@ -14,6 +15,8 @@ mongoose.connect(urlDB)
 routes.forEach((route, index) => {
  fastify.route(route)
 });
+
+fastify.register(require(‘fastify-swagger’), swagger.options);
 
 fastify.get('/', async (request, reply) => {
   return { hello: 'world' }
